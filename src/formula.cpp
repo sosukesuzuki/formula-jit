c829629004086ecd8289273d607a8cef41ef4ec6
Sosuke Suzuki <aosukeke@gmail.com>
2024-05-24 17:22:52 +0900

    Read input

--- src/formula.cpp
+++ src/formula.cpp
@@ -2,12 +2,40 @@
 #include "parser/parser.h"
 #include "vm/bytecodeCompiler.h"
 #include "vm/bytecodeInterpreter.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
-int main()
+// Usage:
+//   1. Read an expression from stdin
+//      $ echo '1 + 1' | formula
+//        2
+//   2. Read an expression from a file
+//      $ echo '1 + 1' > formula-test
+//      $ formula ./formula-test
+//        2
+
+int main(int argc, char* argv[])
 {
-    std::string expression = "3 + 5 * (10 - 2)";
+    bool useJIT = false;
+    std::string expression;
+
+    for (int i = 1; i < argc; ++i) {
+        if (std::string(argv[i]) == "--useJIT")
+            useJIT = true;
+        else {
+            std::ifstream file(argv[i]);
+            if (file.is_open()) {
+                std::stringstream buffer;
+                buffer << file.rdbuf();
+                expression = buffer.str();
+            }
+        }
+    }
+
+    if (expression.empty())
+        std::getline(std::cin, expression);
 
     formula::Lexer lexer(expression);
     auto tokens = lexer.tokenize();
@@ -18,10 +46,13 @@ int main()
     formula::BytecodeCompiler compiler;
     auto bytecode = compiler.compile(ast);
 
-    formula::BytecodeInterpreter interpreter;
-    double result = interpreter.execute(bytecode);
-
-    std::cout << result << std::endl;
+    if (useJIT) {
+        std::cout << "JIT Compiler hasn't been implemented yet" << std::endl;
+    } else {
+        formula::BytecodeInterpreter interpreter;
+        double result = interpreter.execute(bytecode);
+        std::cout << result << std::endl;
+    }
 
     return 0;
 }
