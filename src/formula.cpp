5bb56b1d453455a71ca24f830619338e97e0d9a9
Sosuke Suzuki <aosukeke@gmail.com>
2024-05-24 15:22:00 +0900

    Use all components from formula.cpp

--- src/formula.cpp
+++ src/formula.cpp
@@ -1,7 +1,27 @@
+#include "parser/lexer.h"
+#include "parser/parser.h"
+#include "vm/bytecodeCompiler.h"
+#include "vm/bytecodeInterpreter.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, World" << std::endl;
+    std::string expression = "3 + 5 * (10 - 2)";
+
+    formula::Lexer lexer(expression);
+    auto tokens = lexer.tokenize();
+
+    formula::Parser parser(tokens);
+    auto ast = parser.parse();
+
+    formula::BytecodeCompiler compiler;
+    auto bytecode = compiler.compile(ast);
+
+    formula::BytecodeInterpreter interpreter;
+    double result = interpreter.execute(bytecode);
+
+    std::cout << result << std::endl;
+
     return 0;
 }
